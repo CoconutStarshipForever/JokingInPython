c85311fe81d88e707d340fc36933831df670da4c
Lordobius <tp.mobius1@gmail.com>
2015-01-06 14:29:39 -0500

    add some pycharm stuff and test

--- game.py
+++ game.py
@@ -1,5 +1,4 @@
 __author__ = 'lordobius'
-import pygame
 from Game import engine
 from Game import shape
 from Game import world
@@ -11,19 +10,43 @@ le_monde = world.World(None, "Le_Monde_de_Jean_Claude")
 
 le_jeu = engine.GameEngine(False, (700,500), 60, "Yolo")
 
-def lel():
+class myThread (Thread):
+    def __init__(self, threadID, name, counter, message):
+        Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.counter = counter
+        self.message = message
+    def run(self):
+        print("Starting " + self.name)
+        lel(self.counter, self.message)
+        print("Exiting " + self.name)
+
+class engineMainThread (Thread):
+    def __init__(self, threadID, name, gameEngine):
+        Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.engine = gameEngine
+    def run(self):
+        print("Starting " + self.name)
+        self.engine.start()
+        print("Exiting " + self.name)
+
+def lel(duration, message):
     x = 0
-    print "test"
-    while(x is not 30):
-        print "lol"
+    print("Starting : " + message)
+    while(x is not duration):
+        print(message)
         x += 1
         sleep(1)
 
 
 if __name__ == "__main__":
-    threadTest = Thread(target = lel())
-    thread = Thread(target = le_jeu.start())
-    thread.start()
+    threadTest = myThread(1, "Test 1", 5, "This is test 1")
+    threadTest2 = myThread(2, "Test 2", 5, "This is test 2")
+    threadGameEngine = engineMainThread(3, "The current Game Engine", le_jeu)
+
+    threadTest2.start()
     threadTest.start()
-    threadTest.join()
-    thread.join()
+    threadGameEngine.start()
